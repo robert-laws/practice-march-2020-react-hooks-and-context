0ba4cc7ea0c664e4cfb9ad7880df0e475a128e55
Robert Laws <robertlaws@Roberts-MacBook-Pro.local>
2020-02-29 19:05:03 +0300

    add team data

---
+++ src/data/team.js
@@ -0,0 +1,32 @@
+export const team = [
+  {
+    id: 1,
+    name: 'Bob Cobb',
+    age: 23,
+    fullTime: true
+  },
+  {
+    id: 2,
+    name: 'Hal Hope',
+    age: 52,
+    fullTime: false
+  },
+  {
+    id: 3,
+    name: 'Jim Jupe',
+    age: 38,
+    fullTime: true
+  },
+  {
+    id: 4,
+    name: 'Mel Mope',
+    age: 29,
+    fullTime: true
+  },
+  {
+    id: 5,
+    name: 'Don Dome',
+    age: 44,
+    fullTime: false
+  },
+]
